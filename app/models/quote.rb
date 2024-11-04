0f298bb8066a9715319941bd2fc60a488e0216a1
rokosabic <roko.sabic@gmail.com>
2024-11-04 12:41:29 +0100

    Add new features

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,10 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
+
   validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) }
+
+  broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend
 end
